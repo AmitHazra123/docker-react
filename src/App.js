0b8a944c284883985a933cd98a716ec0f4333f4b
Amit Hazra <amit@muxemail.com>
2020-11-22 18:31:36 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was Changed on the feature branch
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
